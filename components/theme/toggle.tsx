80dceb947b17cf9f97e49f8966cf2e3c06884f23
Yujinndev <xyujinwho@gmail.com>
2025-01-06 11:40:45 +0800

    feat: add themes, dark and light mode

---
+++ components/theme/toggle.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import * as React from 'react'
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import { Button } from '@/components/ui/button'
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <Button
+      variant="outline"
+      size="lg"
+      className="flex w-full justify-start gap-6 p-4"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
+      {theme === 'light' ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      )}
+
+      <p className="capitalize">{theme} theme</p>
+    </Button>
+  )
+}
