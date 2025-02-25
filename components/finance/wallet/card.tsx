940d5bf14e82e0dc0a3f299bed564ee25cd2dbf2
Yujinndev <xyujinwho@gmail.com>
2025-02-25 23:42:06 +0800

    chore: update wallet card props and implement new utils

--- components/finance/wallet/card.tsx
+++ components/finance/wallet/card.tsx
@@ -1,19 +1,10 @@
 import { cn } from '@/lib/utils'
-import { COLORSCHEMES, WALLET_TYPES } from '@/constants/choices'
+import { Wallet } from '@/types/wallet'
+import { getColorScheme, getWalletType } from '@/utils/get-values-from-choices'
 
-interface Wallet {
-  id?: string
-  name: string
-  color?: string | null
-  description?: string | null
-  balance: number | string
-  type: string
-  isDefault: boolean
-}
-
-export const WalletCard = ({ details }: { details: Wallet }) => {
-  const type = WALLET_TYPES.find((type) => type.value === details.type)
-  const color = COLORSCHEMES.find((color) => color.value === details.color)
+export const WalletCard = ({ wallet }: { wallet: Wallet }) => {
+  const color = getColorScheme(wallet?.color)
+  const type = getWalletType(wallet?.type)
 
   return (
     <div
@@ -32,13 +23,13 @@ export const WalletCard = ({ details }: { details: Wallet }) => {
       <div className={cn('polygon absolute inset-0 w-full', color?.accent)} />
       <div className="z-30">
         <h2 className="font-semibold">
-          {details.name === '' ? 'Wallet Name' : details.name}
+          {wallet?.name === '' ? 'Wallet Name' : wallet?.name}
         </h2>
         <p className="font-mono text-base">
-          {details.id?.slice(11, 13)}** **** ****
+          {wallet?.id?.slice(11, 13)}** **** ****
         </p>
 
-        {/* {details.description ?? ''} */}
+        {/* {wallet.description ?? ''} */}
 
         <div className="flex justify-between pt-8">
           <div>
@@ -47,7 +38,7 @@ export const WalletCard = ({ details }: { details: Wallet }) => {
               {new Intl.NumberFormat('en-US', {
                 style: 'currency',
                 currency: 'Php',
-              }).format(parseFloat(details.balance.toString()))}
+              }).format(parseFloat(wallet?.balance?.toString()))}
             </h2>
           </div>
           <div className="flex flex-col items-end justify-center">
