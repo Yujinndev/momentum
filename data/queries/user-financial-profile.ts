3c19c963ef5ff8d43f855014dc2474df92eacbea
Yujinndev <xyujinwho@gmail.com>
2025-04-22 23:47:32 +0800

    chore: remove actions related to financial profile

--- data/queries/user-financial-profile.ts
+++
@@ -1,9 +0,0 @@
-import { getUserFinancialProfile } from '@/actions/account/get-user-financial-profile'
-import { useQuery } from '@tanstack/react-query'
-
-export const useUserFinancialProfile = () => {
-  return useQuery({
-    queryKey: ['financial-profle'],
-    queryFn: getUserFinancialProfile,
-  })
-}
