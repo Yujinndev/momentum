a741c50bcef08b25237be2d84263e8f6728f2334
Yujinndev <xyujinwho@gmail.com>
2025-05-28 23:57:21 +0800

    refactor: update sidebar navigations to groups

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,37 +1,25 @@
 'use client'
 
-import * as React from 'react'
 import Link from 'next/link'
-import { useSession } from 'next-auth/react'
-import { usePathname } from 'next/navigation'
-import { LogOut, Settings } from 'lucide-react'
-import { logout } from '@/actions/account/auth.action'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { NAVIGATIONS } from '@/constants/navigation'
-import { Calendar } from '@/components/ui/calendar'
-import { Button } from '@/components/ui/button'
-import { Brand } from '@/components/brand'
+import * as React from 'react'
 import { cn } from '@/lib/utils'
+import { format } from 'date-fns'
+import { usePathname } from 'next/navigation'
+import { NAVIGATIONS } from '@/constants/navigation'
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
-  SidebarGroupContent,
   SidebarGroupLabel,
-  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarTrigger,
   useSidebar,
 } from '@/components/ui/sidebar'
-import Header from './header'
-import { format } from 'date-fns'
 
 export function AppSidebar(props: React.ComponentProps<typeof Sidebar>) {
   const pathname = usePathname()
-  const { data: session } = useSession()
   const { isMobile } = useSidebar()
 
   return (
@@ -80,48 +68,6 @@ export function AppSidebar(props: React.ComponentProps<typeof Sidebar>) {
           </SidebarGroup>
         ))}
       </SidebarContent>
-      {/*
-      <SidebarFooter>
-        <SidebarGroup className="space-y-1">
-          <SidebarGroupLabel>Settings</SidebarGroupLabel>
-          {session && session.user && (
-            <div className="space-y-2 rounded-md bg-background/20 px-3 py-2">
-              <div className="flex items-center gap-2 p-2 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage
-                    src={session.user.image!}
-                    alt={session.user.name!}
-                  />
-                  <AvatarFallback className="rounded-lg">User</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">
-                    {session.user.name}
-                  </span>
-                  <span className="truncate text-xs">{session.user.email}</span>
-                </div>
-              </div>
-              <div className="flex items-center gap-2">
-                <Button variant="outline" asChild>
-                  <Link href="/account">
-                    <Settings className="text-foreground" />
-                  </Link>
-                </Button>
-                <form action={logout} className="w-full">
-                  <Button
-                    variant="destructive"
-                    className="w-full"
-                    type="submit"
-                  >
-                    <LogOut className="size-4" />
-                    Logout
-                  </Button>
-                </form>
-              </div>
-            </div>
-          )}
-        </SidebarGroup>
-      </SidebarFooter> */}
     </Sidebar>
   )
 }
--- constants/navigation.ts
+++ constants/navigation.ts
@@ -2,7 +2,7 @@ import {
   type LucideIcon,
   SquareTerminal,
   ReceiptText,
-  User,
+  PiggyBank,
 } from 'lucide-react'
 
 type Navigation = {
@@ -13,28 +13,43 @@ type Navigation = {
   isVisible: boolean
 }
 
-export const NAVIGATIONS: Navigation[] = [
+type GroupNavigation = {
+  label: string
+  items: Navigation[]
+}
+
+export const NAVIGATIONS: GroupNavigation[] = [
   {
-    name: 'Dashboard',
-    description: 'Keep track, Assess, and Enhance your financial momentum',
-    url: '/dashboard',
-    icon: SquareTerminal,
-    isVisible: true,
+    label: 'Menu',
+    items: [
+      {
+        name: 'Dashboard',
+        description: 'Keep track, Assess, and Enhance your financial momentum',
+        url: '/dashboard',
+        icon: SquareTerminal,
+        isVisible: true,
+      },
+    ],
   },
   {
-    name: 'Transactions',
-    description:
-      'Stay informed and in control of your spending and income history.',
-    url: '/transactions',
-    icon: ReceiptText,
-    isVisible: true,
-  },
-  {
-    name: 'Account Settings',
-    description:
-      'Stay informed and in control of your spending and income history.',
-    url: '/account',
-    icon: User,
-    isVisible: false,
+    label: 'Finance',
+    items: [
+      {
+        name: 'Transactions',
+        description:
+          'Stay informed and in control of your spending and income history.',
+        url: '/finance',
+        icon: ReceiptText,
+        isVisible: true,
+      },
+      {
+        name: 'Savings',
+        description:
+          'Stay informed and in control of your spending and income history.',
+        url: '/savings',
+        icon: PiggyBank,
+        isVisible: true,
+      },
+    ],
   },
 ]
