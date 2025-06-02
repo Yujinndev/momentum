636c128bd7a7977d1efc1107cad4210ed1b3a705
Yujinndev <xyujinwho@gmail.com>
2025-06-02 23:26:56 +0800

    chore: redesign landing page and auth page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,67 +1,93 @@
 'use client'
 
-import { signinWithGoogle } from '@/actions/account/auth.action'
+import Image from 'next/image'
+import { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
-import { Brand } from '@/components/brand'
-import { Button } from '@/components/ui/button'
-import { getErrorMessage } from '@/utils/error-handler'
+import consistency from '@/public/images/consistency.png'
+import { signinWithGoogle } from '@/actions/account/auth.action'
 import { Alert, AlertDescription } from '@/components/ui/alert'
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+import { getErrorMessage } from '@/utils/error-handler'
+import { Button } from '@/components/ui/button'
+import { Brand } from '@/components/brand'
+import { SectionLayout } from '@/components/layout/section-layout'
+import { ThemeToggle } from '@/components/theme/toggle'
 
-export default function SignIn() {
+const SignInError = () => {
   const searchParams = useSearchParams()
   const error = searchParams.get('error')
 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="space-y-3">
-        {error && (
-          <Alert variant="destructive">
-            <AlertDescription>
-              <span>{getErrorMessage(error)}</span>
-            </AlertDescription>
-          </Alert>
-        )}
-        <Card className="space-y-1 px-4 py-6 shadow-none">
-          <Brand showThemeToggle={true} />
+    <>
+      {error && (
+        <Alert variant="destructive">
+          <AlertDescription>
+            <span>{getErrorMessage(error)}</span>
+          </AlertDescription>
+        </Alert>
+      )}
+    </>
+  )
+}
 
-          <CardHeader className="text-center">
-            <CardTitle className="text-xl">Welcome back</CardTitle>
-            <CardDescription>Login with your Google account</CardDescription>
-          </CardHeader>
+export default function SignIn() {
+  return (
+    <div className="relative flex h-dvh overflow-hidden bg-background p-2">
+      <div className="flex min-w-[50%] flex-1 flex-col items-center justify-center gap-12 px-8 py-6 text-foreground">
+        <div className="absolute left-8 top-8">
+          <ThemeToggle />
+        </div>
 
-          <form action={signinWithGoogle} className="px-8">
-            <Button variant="secondary" className="w-full">
-              <div className="flex w-full items-center justify-center gap-4">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path
-                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                    fill="currentColor"
-                  />
-                </svg>
-                <span>Login with Google</span>
-              </div>
+        <Suspense>
+          <SignInError />
+        </Suspense>
+
+        <Brand className="w-full justify-center border-0 text-3xl" />
+
+        <SectionLayout className="mx-auto h-max w-5/6 max-w-sm">
+          <form action={signinWithGoogle} className="space-y-4">
+            <div>
+              <h1 className="text-xl font-bold text-foreground">
+                Welcome back
+              </h1>
+              <p>Login with your Google account</p>
+            </div>
+
+            <Button
+              size="lg"
+              className="btn-primary flex w-full items-center justify-center"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path
+                  d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                  fill="currentColor"
+                />
+              </svg>
+
+              <span>Login with Google</span>
             </Button>
-          </form>
 
-          <p className="mx-auto w-4/5 text-center text-sm/6">
-            <span>By clicking continue, you agree to our </span>
-            <a href="#" className="underline underline-offset-8">
-              Terms of Service
-            </a>{' '}
-            and{' '}
-            <a href="#" className="underline underline-offset-8">
-              Privacy Policy
-            </a>
-            .
-          </p>
-        </Card>
+            <p className="text-center text-sm/6">
+              <span>By clicking continue, you agree to our </span>
+              <a href="#" className="underline underline-offset-8">
+                Terms of Service
+              </a>{' '}
+              and{' '}
+              <a href="#" className="underline underline-offset-8">
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </form>
+        </SectionLayout>
       </div>
+
+      <Image
+        src={consistency}
+        alt="logo"
+        width={400}
+        height={1080}
+        className="hidden flex-1 rounded-lg object-cover lg:block"
+      />
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,39 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { Brand } from '@/components/brand'
 import { Button } from '@/components/ui/button'
+import { ThemeToggle } from '@/components/theme/toggle'
+import consistency from '@/public/images/consistency.png'
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <div className="mx-auto flex w-3/5 items-center justify-between gap-8 border-b pb-8">
-          <div className="w-full">
-            <Brand className="w-full justify-center border-0 text-3xl" />
-            <Button variant="secondary" className="px-6" asChild>
-              <Link href="/auth/signin">Start stepping up!</Link>
-            </Button>
-          </div>
-          <p className="text-right text-base font-thin text-muted-foreground">
+    <div className="relative flex h-dvh w-full overflow-hidden bg-background p-2">
+      <div className="flex min-w-[50%] flex-1 flex-col items-center justify-center gap-12 px-8 py-6 text-foreground">
+        <div className="absolute left-8 top-8">
+          <ThemeToggle />
+        </div>
+
+        <Brand className="w-5/6 max-w-sm justify-center border-0 text-3xl" />
+
+        <div className="mx-auto w-5/6 max-w-sm space-y-8 text-center">
+          <p className="text-center text-base font-thin text-foreground">
             Momentum is your all-in-one app for managing finances, tracking
             habits, and achieving life goals. Gain clarity, build better habits,
             and take charge of your personal and financial growth with Momentum.
           </p>
+          <Button size="lg" className="btn-primary w-full" asChild>
+            <Link href="/auth/signin">Start stepping up!</Link>
+          </Button>
         </div>
-      </main>
+      </div>
+
+      <Image
+        src={consistency}
+        alt="logo"
+        width={400}
+        height={1080}
+        className="hidden flex-1 rounded-lg object-cover lg:block"
+      />
     </div>
   )
 }
