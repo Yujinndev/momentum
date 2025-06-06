dcb5b49c8ecfcf98b315f1c6006ec5605a7f51fe
Yujinndev <xyujinwho@gmail.com>
2025-06-06 23:47:25 +0800

    chore: add multiplier in getRecurringPeriodDate for multi schedules

--- utils/get-recurring-period-date.ts
+++ utils/get-recurring-period-date.ts
@@ -3,29 +3,31 @@ import { addDays } from 'date-fns'
 type GetRecurringPeriodArgs = {
   startDate: Date
   period: 'DAILY' | 'WEEKLY' | 'MONTHLY' | 'ANNUALLY' | 'NONE'
+  multiplier?: number
 }
 
 export const getRecurringPeriodDate = ({
   startDate,
   period,
+  multiplier = 1,
 }: GetRecurringPeriodArgs) => {
   switch (period) {
     case 'DAILY':
-      return addDays(startDate, 1)
+      return addDays(startDate, 1 * multiplier)
 
     case 'WEEKLY':
-      return addDays(startDate, 7)
+      return addDays(startDate, 7 * multiplier)
 
     case 'NONE':
-      return addDays(startDate, 30)
+      return addDays(startDate, 30 * multiplier)
 
     case 'MONTHLY':
-      return addDays(startDate, 30)
+      return addDays(startDate, 30 * multiplier)
 
     case 'ANNUALLY':
-      return addDays(startDate, 365)
+      return addDays(startDate, 365 * multiplier)
 
     default:
-      return addDays(startDate, 30)
+      return addDays(startDate, 30 * multiplier)
   }
 }
